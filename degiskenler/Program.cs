becb1a0a7d2c7818b8f7e0fff5b9b715f9490af6
omerfarukkaya590 <85821993+omerfarukkaya590@users.noreply.github.com>
2021-11-19 15:59:48 +0300

    Add files via upload

---
+++ degiskenler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            byte b =5;
+            sbyte c =5;
+
+            short d =5;
+            ushort e =5;
+
+            Int16 i16 =2;
+            int i =2;
+            Int32 i32 =2;
+            Int64 i64 =2;
+
+            uint ui =2;
+            long l =4;
+            ulong ul =4;
+
+            float f =5;
+            double db =5;
+            decimal de =5;
+
+            char ch =2;
+            stringg str ="Omer";
+
+            bool b1 =true;
+            bool b2 =false;
+
+            DateTime dt =DateTime.Now;
+            Console.Writline(dt);
+        }
+    }
+}
